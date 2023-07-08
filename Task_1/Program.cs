92dc5a27962c980f791bcecc8d2e5efa5e4b47d0
XTroll <xtroll@inbox.ru>
2023-07-09 02:47:09 +1000

    C#_Homework_9

---
+++ Task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66:
+// Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// // M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+void Main()
+{
+    int M = ReadInt("Введите начальное значение M: ");
+    int N = ReadInt("Введите конечное значение N: ");
+    if (M < N)
+    {
+       System.Console.WriteLine (Sum (M, N));
+    }
+    else System.Console.WriteLine (Sum (N, M));
+}
+
+Main();
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int M, int N)
+{
+    if (M == N) return M;
+    return M + Sum (M + 1, N);
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 68:
+// Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+using System.Threading;
+
+Thread T = new Thread(Main, 16777216); //16 Mb стэк работает до значений 4,1 и 3,14
+T.Start();
+
+void Main()
+{
+    
+    int M = ReadInt("Введите значение M: ");
+    int N = ReadInt("Введите значение N: ");
+    System.Console.WriteLine($" {Akkerman (M, N)}");
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Akkerman(int M, int N)
+{
+    if (M == 0) return N + 1;
+    else if (M > 0 && N == 0) return Akkerman(M - 1, 1);
+    else return Akkerman (M - 1, Akkerman (M, N - 1));
+}
